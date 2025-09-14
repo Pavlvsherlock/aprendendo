b5d2cd750c902fc7c6afcb4bc07228320bb906ae
Pavlvsherlock <p.abalo.perez@gmail.com>
2025-09-14 13:01:32 +0200

    domingo chuvioso

--- libroC/ConsoleApplication1/hola.c
+++ libroC/ConsoleApplication1/hola.c
@@ -30,15 +30,16 @@ int main()
     scanf("%d",&p1.edad);
     printf("Canto mides? ");
     scanf("%f", &p1.altura);
-    printf("Hola %s tes %d anos e mides %.2f",p1.nombre, p1.edad, p1.altura);
+    printf("Hola %s tes %d anos e mides %.2f metros",p1.nombre, p1.edad, p1.altura);
 
     //instancia de Libros
-    Libros l1;
-    strcpy(l1.titulo, "El señor de los anillos");
+   /* Libros l1;
+   strcpy(l1.titulo, "El señor de los anillos");
     strcpy(l1.autor, "J.R.R Tolkien");
     l1.paginas=957;
     l1.precio=52.99;
-
+    */
+    Libros l1={"the lord of the rings", "tolkien", 50, 25};
     //imprimimos datos do libro
     printf("\nfalamos do libro %s, escrito por %s. %s ten %d paginas e costa %.2f euros", l1.titulo, l1.autor, l1.titulo, l1.paginas, l1.precio);
     return 0;
