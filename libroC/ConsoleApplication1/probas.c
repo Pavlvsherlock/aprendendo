2c967e2a37105f63f15ab1364f611f87d2b939be
Pavlvsherlock <p.abalo.perez@gmail.com>
2025-09-14 13:26:26 +0200

    probas.c parte 2

--- libroC/ConsoleApplication1/probas.c
+++ libroC/ConsoleApplication1/probas.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void cambia(int n1 ,int  n2);
+void cambia(int* n1 ,int*  n2);
 int main(void){
 
 int num1=1, num2=2;
@@ -11,7 +11,7 @@ printf("o numero 2 vale: %d\n", num2);
     return 0;
 }
 void cambia(int* n1, int* n2){
-    int comodin=n1;
-    n1=n2;
-    n2=comodin;
+    int comodin= *n1;
+    *n1= *n2;
+    *n2=comodin;
 }
