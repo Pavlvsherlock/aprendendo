0a479506604f647c98a71b71f883ca33a96d1655
Pavlvsherlock <p.abalo.perez@gmail.com>
2025-09-14 13:22:12 +0200

    probas.c

---
+++ libroC/ConsoleApplication1/probas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void cambia(int n1 ,int  n2);
+int main(void){
+
+int num1=1, num2=2;
+cambia(&num1, &num2);
+
+
+printf("O numero 1 vale: %d\n", num1);
+printf("o numero 2 vale: %d\n", num2);
+    return 0;
+}
+void cambia(int* n1, int* n2){
+    int comodin=n1;
+    n1=n2;
+    n2=comodin;
+}
